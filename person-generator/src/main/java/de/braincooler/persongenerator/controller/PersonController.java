1595f97d7215fbd5745d55537cf2aea6a39876a1
WZeller <w.zeller@online.de>
2021-10-27 17:14:43 +0200

    add new route

--- person-generator/src/main/java/de/braincooler/persongenerator/controller/PersonController.java
+++ person-generator/src/main/java/de/braincooler/persongenerator/controller/PersonController.java
@@ -1,7 +1,8 @@
 package de.braincooler.persongenerator.controller;
 
+import de.braincooler.persongenerator.controller.dto.PersonDto;
+import de.braincooler.persongenerator.service.PersonService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,23 +12,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import de.braincooler.persongenerator.controller.dto.PersonDto;
-import de.braincooler.persongenerator.service.PersonService;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "persons")
 @RequiredArgsConstructor
 public class PersonController {
-	private final PersonService personService;
-	
-	@GetMapping(value = "person", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<PersonDto> createPerson(){
-		return ResponseEntity.ok(personService.createPerson());
-	}
-	
-	@DeleteMapping(value = "person/{id}")
-	public ResponseEntity<?> deletePerson(@PathVariable Long id){
-		personService.deletePerson(id);
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+    private final PersonService personService;
+
+    @GetMapping(value = "person", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PersonDto> createPerson() {
+        return ResponseEntity.ok(personService.createPerson());
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<PersonDto>> getAllPersons() {
+        return ResponseEntity.ok(personService.getPersons());
+    }
+
+    @DeleteMapping(value = "person/{id}")
+    public ResponseEntity<?> deletePerson(@PathVariable Long id) {
+        personService.deletePerson(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- person-generator/src/main/java/de/braincooler/persongenerator/service/PersonService.java
+++ person-generator/src/main/java/de/braincooler/persongenerator/service/PersonService.java
@@ -1,11 +1,14 @@
 package de.braincooler.persongenerator.service;
 
+import de.braincooler.persongenerator.controller.dto.PersonDto;
 import de.braincooler.persongenerator.entity.PersonEntity;
 import de.braincooler.persongenerator.generator.PersonGenerator;
-import de.braincooler.persongenerator.controller.dto.PersonDto;
 import de.braincooler.persongenerator.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +27,11 @@ public class PersonService {
 
     public void deletePerson(Long id) {
         personRepository.deleteById(id);
+    }
+
+    public List<PersonDto> getPersons() {
+        return personRepository.findAll().stream()
+                .map(personMapper::personEntityToDto)
+                .collect(Collectors.toList());
     }
 }
